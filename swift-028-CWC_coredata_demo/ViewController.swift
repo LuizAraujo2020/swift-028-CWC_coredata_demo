a5c450d144d2e12e786499e26239b10e674e9f23
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-01 20:33:40 -0300

    Initial Commit

---
+++ swift-028-CWC_coredata_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-028-CWC_coredata_demo
+//
+//  Created by Luiz Carlos da Silva Araujo on 01/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
